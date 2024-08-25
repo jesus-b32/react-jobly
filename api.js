f8762ea5e6f3b02a1ec2d400bcc4606af303bd0f
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-08-24 22:31:17 -0700

    designed component hierarchy and added some static methods to API helper

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 /** API Class.
  *
@@ -14,39 +14,70 @@ class JoblyApi {
   // the token for interactive with the API will be stored here.
   static token;
 
-  static async request(endpoint, data = {}, method = "get") {
-    console.debug("API Call:", endpoint, data, method);
+  static async request(endpoint, data = {}, method = 'get') {
+    console.debug('API Call:', endpoint, data, method);
 
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
-    const params = (method === "get")
-        ? data
-        : {};
+    const params = method === 'get' ? data : {};
 
     try {
       return (await axios({ url, method, data, params, headers })).data;
     } catch (err) {
-      console.error("API Error:", err.response);
+      console.error('API Error:', err.response);
       let message = err.response.data.error.message;
       throw Array.isArray(message) ? message : [message];
     }
   }
 
   // Individual API routes
-
+  //Company API routes //////////////////////////////////////////////////////
+  /** Get list of companies. */
+  static async getCompanies() {
+    const res = await this.request(`companies/`);
+    return res.companies;
+  }
   /** Get details on a company by handle. */
-
   static async getCompany(handle) {
-    let res = await this.request(`companies/${handle}`);
+    const res = await this.request(`companies/${handle}`);
     return res.company;
   }
 
-  // obviously, you'll add a lot here ...
+  ////////////////////////////////////////////////////////////////////////////
+
+  //Job API routes //////////////////////////////////////////////////////
+  /** Get list of jobs. */
+  static async getJobs() {
+    const res = await this.request(`jobs/`);
+    return res.jobs;
+  }
+
+  /** Get details on a job by id. */
+  static async getJob(id) {
+    const res = await this.request(`jobs/${id}`);
+    return res.job;
+  }
+  ////////////////////////////////////////////////////////////////////////////
+
+  //User API routes //////////////////////////////////////////////////////
+  /** Get list of users. */
+  static async getUsers() {
+    const res = await this.request(`users/`);
+    return res.users;
+  }
+
+  /** Get details on a user by username. */
+  static async getUser(username) {
+    const res = await this.request(`users/${username}`);
+    return res.user;
+  }
+  ////////////////////////////////////////////////////////////////////////////
 }
 
 // for now, put token ("testuser" / "password" on class)
-JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+JoblyApi.token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ' +
+  'SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0.' +
+  'FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc';
